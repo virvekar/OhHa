3107a9a2a971dc1fbbd634fc876ca07f1a854a1d
Karsisto Virve E <virvemaa@da1-cs-b221-12.helsinki.fi>
2014-01-16 11:11:05 +0200

    netbeans projekti lisatty

---
+++ tienhoitoPeli/src/main/java/tienhoitopeliid/tienhoitopeli/App.java
@@ -0,0 +1,13 @@
+package tienhoitopeliid.tienhoitopeli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
